d333d14b33cc2f779d91808b58caa7dfd2a4ea2a
Guillaume Campagna <guillaume.campagna@gmail.com>
2019-09-30 17:30:42 -0400

    Fix build

--- include/msgpack/type.hpp
+++ include/msgpack/type.hpp
@@ -10,10 +10,7 @@
 #include "adaptor/int.hpp"
 #include "adaptor/list.hpp"
 #include "adaptor/map.hpp"
-#include "adaptor/nil.hpp"
 #include "adaptor/pair.hpp"
-#include "adaptor/raw.hpp"
-#include "adaptor/v4raw.hpp"
 #include "adaptor/set.hpp"
 #include "adaptor/size_equal_only.hpp"
 #include "adaptor/string.hpp"
@@ -21,7 +18,6 @@
 #include "adaptor/vector_bool.hpp"
 #include "adaptor/vector_char.hpp"
 #include "adaptor/vector_unsigned_char.hpp"
-#include "adaptor/wstring.hpp"
 #include "adaptor/msgpack_tuple.hpp"
 #include "adaptor/define.hpp"
 
@@ -44,14 +40,6 @@
 #include "adaptor/cpp11/unique_ptr.hpp"
 #include "adaptor/cpp11/unordered_map.hpp"
 #include "adaptor/cpp11/unordered_set.hpp"
-
-#if MSGPACK_HAS_INCLUDE(<optional>)
-#include "adaptor/cpp17/optional.hpp"
-#endif // MSGPACK_HAS_INCLUDE(<optional>)
-
-#if MSGPACK_HAS_INCLUDE(<string_view>)
-#include "adaptor/cpp17/string_view.hpp"
-#endif // MSGPACK_HAS_INCLUDE(<string_view>)
 
 #include "adaptor/cpp17/byte.hpp"
 #include "adaptor/cpp17/carray_byte.hpp"
